b33c7ef8471148231d5dede30b25aba59acdb7fa
DevIgorSantos <dsg.igorsantos@gmail.com>
2026-05-27 14:59:54 -0300

    Remove test file

--- test-marketing-perms.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
-
-async function run() {
-  const { data: perms } = await supabase.from('permissions').select('*').ilike('name', 'marketing%');
-  console.log("Marketing permissions:", perms);
-}
-run();
